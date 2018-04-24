10cd9bdfd667b67fa5de0fff391482d3573089be
Daniel Neamati <danineamati@gmail.com>
2018-04-24 11:07:57 -0700

    Made graph selectable

--- Ge_1_Sets/Ge 1 Set 2.py
+++ Ge_1_Sets/Ge 1 Set 2.py
@@ -19,16 +19,10 @@ g = 10
 C = Cp * P / g
 print(C)
 
-
-print(AofTs(288))
-
 ## Setting
 iceOn = True
 waterOn = False
 
-
-########## Problem 1 ##############
-print("########## Problem 1 ##############")
 
 ## Making plot
 matplotlib.rc('xtick',labelsize = 18)
@@ -36,7 +30,7 @@ matplotlib.rc('ytick',labelsize = 18)
 matplotlib.rc('axes',linewidth = 2)
 
 
-def feedbackEarth(dTsdt, lines, surface = False):
+def feedbackEarth(X, Y, dTsdt, lines, surface = False):
     
     fig = plt.figure()
     if surface:
@@ -59,45 +53,46 @@ def feedbackEarth(dTsdt, lines, surface = False):
 
     plt.show()
 
-snowBallEarth(dTsdt1, 15)
-snowBallEarth(dTsdt1, 15, surface = True)
-
-########## Problem 3 ##############
-iceOn = False
-waterOn = True
-
-T0 = np.arange(220, 320, 0.1)
-Ts = np.arange(Tmin, Tmax, 0.1)
-
-X, Y = np.meshgrid(T0, Ts)
-
-dTsdt2 = (1/C)*(sigma*(X**4)*(1 - A0)- (1/(1 + Tau(Y)))*sigma*(Y**4))
-
-snowBallEarth(dTsdt2, 15)
-
 def makeGraph(iceOn, lines, surface = False):
     '''Makes specific of the two plots.'''
     if iceOn:
+        ########## Problem 1 ##############
+        print("########## Problem 1 ##############")
         x = 5
         Tmin, Tmax = 220, 320
         ## Albedo at a given temperature
         AofTs = lambda Ts: 0.45 - 0.25 * np.tanh((Ts - 272)/23)
         ## Constant Opacity (Tau)
         tau = 0.68
-        dTsdt = (1/C)*(sigma*(X**4)*(1 - AofTs(Y))-(1/(1+tau))*sigma*(Y**4))
+        
     else:
+        ########## Problem 3 ##############
+        print("########## Problem 3 ##############")
         x = 0
         Tmin, Tmax = 260, 360
         ## Assume constant albedo
         A0 = 0.3 
         ## Opacity at given temperature (Tau)
         Tau = lambda Ts: 0.55 + 0.07 * np.exp(5413*((1/288) - (1/(Ts))))
+        
 
     T0 = np.arange(220, 320, 0.1)
     Ts = np.arange(Tmin, Tmax, 0.1)
 
     X, Y = np.meshgrid(T0, Ts)
 
+    if iceOn:
+        ## Feedback function
+        dTsdt = (1/C)*(sigma*(X**4)*(1 - AofTs(Y))-(1/(1+tau))*sigma*(Y**4))
+    else:
+        ## Feedback function
+        dTsdt = (1/C)*(sigma*(X**4)*(1 - A0)- (1/(1 + Tau(Y)))*sigma*(Y**4))
+
+    feedbackEarth(X, Y, dTsdt, lines, surface)
+
+    
+
+makeGraph(True, 20)
 
 
 
